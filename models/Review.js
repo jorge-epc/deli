8fb4b4b5fa1e618f1178a6fe949e7bb96ea00ebe
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2017-07-13 12:45:16 -0500

    App

---
+++ models/Review.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const reviewSchema = new mongoose.Schema({
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'User is required'
+  },
+  store: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Store',
+    required: 'Store is required'
+  },
+  text: {
+    type: String,
+    required: 'You must supply a text',
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5
+  }
+});
+
+function autopopulate(next) {
+  this.populate('author');
+  next();
+}
+
+reviewSchema.pre('find', autopopulate);
+reviewSchema.pre('findOne', autopopulate);
+
+module.exports = mongoose.model('Review', reviewSchema);
